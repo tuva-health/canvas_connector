f46dbd3d1ab674f1ecdb2f3749579c2c0744c941
Allen Huang <allen@allenhuang.net>
2025-10-14 15:19:07 +0100

    concatenated and coalesced IDs to get unique identifiers

--- models/intermediate/int_medication.sql
+++ models/intermediate/int_medication.sql
@@ -1,6 +1,6 @@
 with mapped_data as (
     select
-        {{ sha_hash_512('m.id') }} as medication_id 
+        {{ sha_hash_512('concat(m.id, coalesce(pr.id, m.id))') }} as medication_id 
         , p.mrn as person_id
         , p.id as patient_id
         , null as encounter_id
